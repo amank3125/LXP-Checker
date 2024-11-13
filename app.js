eba63b39e1a14d70e25aa1170b91e6be7c4d2b00
Aman PCO <amank3125@gmail.com>
2024-11-13 17:47:42 +0530

    added history and fixed UI

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const addressContainer = document.querySelector('.addressContainer');
 const searchBtn = document.querySelector('.searchBtn');
 const navDiv = document.querySelector('.navDiv');
 const lxpBalance = document.querySelector('.lxpBalance');
+const lxpHistory = document.querySelector('.lxpHistory');
 const errorContainer = document.querySelector('.errorContainer');
 const errorMsg = document.querySelector('.errorMsg');
 const errorClose = document.querySelector('.errorClose');
-const lxpHistory = document.querySelector('.lxpHistory');
+const loader = document.querySelector('.loader');
+const shimmerContainer = document.querySelector('.shimmerContainer');
 let errorState = 0;
 const regex = /^(0x)?[0-9a-fA-F]{40}$/;
 let lxp = 0;
@@ -26,7 +28,20 @@ function hideMenu(){
     hideMenuBtn.classList.add('hidden');
     navDiv.classList.add('hidden');
 }
-
+function resetValues(){
+    lxpHistory.innerHTML = "";
+    lxpBalance.textContent = "";
+    walletAddress.value = "";
+    hideLoaders();
+}
+function callLoaders(){
+    loader.classList.remove('hidden');
+    shimmerContainer.classList.remove('hidden');
+}
+function hideLoaders(){
+    loader.classList.add('hidden');
+    shimmerContainer.classList.add('hidden');
+}
 errorClose.addEventListener('click', () => { // close error message
     errorContainer.style.transform='translateY(-250%)';
     errorContainer.style.filter='opacity(0)';
@@ -41,36 +56,52 @@ searchBtn.addEventListener('click',()=>{
     const walletAdd = walletAddress.value.trim();
       if (!walletAdd) {
         callError('Invalid wallet address!');
-        console.log('err')
+        invalidAddress();
         return;
       } else if (regex.test(walletAdd)) {
         console.log('calling fetchData()')
           fetchData();
       }
    else {
+    invalidAddress();
     callError('Invalid wallet address!')
   }});
 
 async function fetchData(){
+    callLoaders()
     const walletAdd = walletAddress.value.trim();
     const encodedAddress = encodeURIComponent(walletAdd);
 
     await fetch(`/.netlify/functions/getLXP?addr=${encodedAddress}`).then(resp=>resp.json()).then(data=>{
-        console.log(data)
+        hideLoaders();
         lxpBalance.textContent=(data.result/10**18).toFixed();
-    }).catch(err=>console.log(err));
+    }).catch(err=>{callError(err);console.log(err)});
 
-    await fetch(`/.netlify/functions/getHistory?addr=${encodedAddress}`).then(resp=>resp.json()).then(data=>console.log("Historical Data: ",data)).catch(err=>console.log(err));
+    await fetch(`/.netlify/functions/getHistory?addr=${encodedAddress}`).then(resp=>resp.json()).then(data=>{
+        let dates = [];
+        let value = [];
+        let txnHash = [];
+        lxpHistory.innerHTML = ""
+        for(i=0;i<data.result.length;i++){
+        // dates.push(data.result[i].timestamp);
+        // value.push(data.result[i].value);
+        // txnHash.push(data.result[i].hash);
+        hideLoaders();
+        lxpHistory.innerHTML += `<a href="https://lineascan.build/tx/${data.result[i].hash}" target="_blank"><div class="lxpCard"><p class="lxpCardDate">${new Date(data.result[i].timeStamp*1000).toLocaleDateString()}</p><div class="innerCardDiv"><img class="lxpTokenImg" src="./src/Linea Logos/Logomark Blue BG.png"></img><span class="lxpAmount">${(data.result[i].value/10**18).toFixed()}</span></div></div></a>`;
+        }
+    }).catch(err=>{callError(err);console.log(err)});
 };
-
-function callError(e){  // show error message
+function invalidAddress(){
     errorState = 1;
-    // errorMsg.textContent=e;
-    // errorContainer.classList.remove('hidden');
-    // errorContainer.style.transform='translateY(0)';
-    // errorContainer.style.filter='opacity(1)';
     addressContainer.style.border = "2px solid var(--red)";
     walletAddress.placeholder = "Invalid Wallet address!";
+}
+
+function callError(e){  // show error message
+    errorMsg.textContent=e;
+    errorContainer.classList.remove('hidden');
+    errorContainer.style.transform='translateY(0)';
+    errorContainer.style.filter='opacity(1)';
 }
 walletAddress.addEventListener('input',()=>{
     if(errorState!=0){
--- netlify/functions/getHistory.js
+++ netlify/functions/getHistory.js
@@ -3,7 +3,7 @@ const addr = event.queryStringParameters && event.queryStringParameters.addr;
 const secret = process.env.lineaKey;
     try {
         const { default: fetch } = await import('node-fetch');
-        const response = await fetch(`https://api.lineascan.build/api?module=account&action=tokentx&contractaddress=0xd83af4fbD77f3AB65C3B1Dc4B38D7e67AEcf599A&address=${encodeURIComponent(addr)}&page=1&offset=1000&startblock=0&endblock=270257800&sort=asc&apikey=${secret}`);
+        const response = await fetch(`https://api.lineascan.build/api?module=account&action=tokentx&contractaddress=0xd83af4fbD77f3AB65C3B1Dc4B38D7e67AEcf599A&address=${encodeURIComponent(addr)}&page=1&offset=1000&startblock=0&endblock=270257800&sort=desc&apikey=${secret}`);
         const data = await response.json();
     return {
         statusCode: 200,
